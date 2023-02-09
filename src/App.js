46432c303db93dbb810ee68ed79eab1dec075c9e
Vikas Singh <xanderbilla@gmail.com>
2023-02-09 19:12:56 +0200

    Component Updated

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import About from './components/about/About'
 import Contact from './components/contact/Contact'
 import { Intro } from './components/intro/Intro'
 import ProjectList from './components/projectsList/ProjectList'
+import Toggle from './components/toggle/Toggle'
+import { ThemeContext } from './Context'
 
 const App = () => {
+
+  const theme = useContext(ThemeContext);
+  const darkMode = theme.state.darkMode;
+
   return (
-    <div>
+    <div style={{backgroundColor:darkMode ? "#222" : "white", color: darkMode && "white"}}>
+      <Toggle/>
       <Intro />
       <About />
       <ProjectList />
