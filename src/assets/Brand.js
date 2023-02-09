6b7706189fae0470d64b027a00324d92a98c7b4e
Vikas Singh <xanderbilla@gmail.com>
2023-02-09 19:35:14 +0200

    Brand Logo

---
+++ src/assets/Brand.js
@@ -0,0 +1,13 @@
+function Brand(props) {
+    return (
+        <img
+            src="https://i.imgur.com/lJ7QosV.png"
+            width={props.width}
+            height={props.height}
+            className="d-inline-fixed align-top"
+            alt="logo"
+        />
+    );
+}
+
+export default Brand;
