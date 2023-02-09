86b2a26c8b3002a6c3bb62f4e24361c7d66d0585
Vikas Singh <xanderbilla@gmail.com>
2023-02-09 15:52:08 +0200

    Added data for Project Section

---
+++ src/Data.js
@@ -0,0 +1,32 @@
+export const products = [
+    {
+        id: 1,
+        img: "https://img.freepik.com/free-vector/portrait-beautiful-girl-vector-cartoon-illustration_1196-946.jpg",
+        link: "https://xanderbilla.vercel.app"
+    },
+    {
+        id: 2,
+        img: "https://img.freepik.com/free-vector/portrait-beautiful-girl-vector-cartoon-illustration_1196-946.jpg",
+        link: "https://xanderbilla.vercel.app"
+    },
+    {
+        id: 3,
+        img: "https://img.freepik.com/free-vector/portrait-beautiful-girl-vector-cartoon-illustration_1196-946.jpg",
+        link: "https://xanderbilla.vercel.app"
+    },
+    {
+        id: 4,
+        img: "https://img.freepik.com/free-vector/portrait-beautiful-girl-vector-cartoon-illustration_1196-946.jpg",
+        link: "https://xanderbilla.vercel.app"
+    },
+    {
+        id: 5,
+        img: "https://img.freepik.com/free-vector/portrait-beautiful-girl-vector-cartoon-illustration_1196-946.jpg",
+        link: "https://xanderbilla.vercel.app"
+    },
+    {
+        id: 6,
+        img: "https://img.freepik.com/free-vector/portrait-beautiful-girl-vector-cartoon-illustration_1196-946.jpg",
+        link: "https://xanderbilla.vercel.app"
+    },
+]  
