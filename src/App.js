dcc34821fd70bd65e39f4b6c1618dccf4b133c3f
Vikas Singh <xanderbilla@gmail.com>
2023-02-09 15:51:41 +0200

    Updated App Component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React from 'react'
 import About from './components/about/About'
+import Contact from './components/contact/Contact'
 import { Intro } from './components/intro/Intro'
+import ProjectList from './components/projectsList/ProjectList'
 
 const App = () => {
   return (
-      <div>
+    <div>
       <Intro />
       <About />
+      <ProjectList />
+      <Contact />
     </div>
   )
 }
