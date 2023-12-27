a6bc69678c1bf0deca6cbc63f2390852db0aa73e
Vikas Singh <33988028+xanderbilla@users.noreply.github.com>
2023-12-27 20:06:44 +0530

    Update Brand.js

--- src/assets/Brand.js
+++ src/assets/Brand.js
@@ -1,7 +1,7 @@
 function Brand(props) {
     return (
         <img
-            src="https://i.imgur.com/lJ7QosV.png"
+            src="https://i.imgur.com/S5awLwi.png"
             width={props.width}
             height={props.height}
             className="d-inline-fixed align-top"
